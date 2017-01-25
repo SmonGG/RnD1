0836ecb9ba9f6c40d4dc12581a7836ed0f4d0688
Simon Goode <simongoode95@gmail.com>
2017-01-25 14:26:06 +1100

    dndE_v3 fix. rv_continuous high processing time for 'rvs' function
    After implementing most of vel_dist.py into dndE_v3.py, the result is a
    working program, however not as intended. It was discovered that
    utilising the rvs command with my_dmdv causes a tremendous amount of
    processing time. 2 samples usually takes ~7 seconds to process, 10
    samples is ~20 seconds and anything beyond 100 samples is unfathomable.
    Also worth mentioning is the problem with passing units in and out of
    my_dmdv; somehow the output of the function strips the values of their
    units.

--- dndE_v3.py
+++ dndE_v3.py
@@ -32,21 +32,27 @@ class HALOclass(object):
 		self.v_min=v_min * units.km / units.s
 		self.v_max=v_max * units.km / units.s
 
-
-mass = 10. ##(float(input("Enter the mass of dark matter (GeV): ")))
+mass = 10.
 v = 1.
 
-# DM halo properties
 v_min = 1.
-v_max = 3.
-temp = (0.5 * mass * (230. * units.km/units.s)**2)/constants.k_B
-print "Effective temp for DM particles moving at 230km/s ", temp.to(units.Kelvin)
-a = np.sqrt(constants.k_B * temp/mass)
-print "Normalisation velocity ",a
-scale = a
+v_max = 800.
+temp = ((0.5 * mass * (v * units.km/units.s)**2)/constants.k_B).cgs *units.g
 
 DM = DMclass(mass,v,temp)
 MW = HALOclass(v_min,v_max)
+
+print DM.mass ##(float(input("Enter the mass of dark matter (GeV): ")))
+print DM.temp
+print DM.v
+print MW.v_min
+print MW.v_max
+
+# DM halo properties
+print "Effective temp for DM particles moving at ",DM.v, ":", temp.to(units.Kelvin)
+a = np.sqrt(constants.k_B * temp/mass)
+print "Normalisation velocity ",a
+scale = a
 
 m = DM.mass
 k_B = constants.k_B
@@ -71,6 +77,7 @@ class dmdv_newgen(rv_continuous):
 		T=T * units.Kelvin		
 		k_B = constants.k_B
 		out = np.sqrt((m/(2.*np.pi*k_B*T))**3) * (4.*np.pi*(v**2)) * (np.exp(-(m*(v**2))/(2.*k_B*T)))
+		out = out.cgs
 		out = out.to(units.s/units.km)
 		return out.value
 
@@ -82,8 +89,15 @@ print "Out to 100 km/s not many particles ", my_dmdv.cdf(100., m=DM.mass, T=DM.t
 print "Out to 300 km/s about half of all particles ", my_dmdv.cdf(300., m=DM.mass, T=DM.temp)
 print "Out to 600 km/s should include most of all particles ", my_dmdv.cdf(600., m=DM.mass, T=DM.temp)
 
-values = my_dmdv.rvs(m=DM.mass, T=DM.temp, size=100)
-plt.hist(values,bins=500)
+vrange = 10.*(np.linspace(MW.v_min,MW.v_max,num=10000.))
+
+rv = my_dmdv.pdf(vrange, m=DM.mass.value, T=DM.temp.value, loc=0, scale=scale)
+plt.plot(vrange,rv,'-',color='b')
+
+v_values = my_dmdv.rvs(m=DM.mass.value, T=DM.temp.value, size=2, loc=0, scale=scale)
+plt.hist(v_values,2,color='b',normed=1)
+print v_values
+plt.show()
 
 #note: rvs function returns a duple. when assigning a variable that uses the rvs command, make sure that you split it: eg. test_v, test_DF = rvs(...)
 # #Noise Emulator
@@ -93,13 +107,13 @@ def noisy(vdf, mu=0, sigma=0.5):
 	return soln
 # #	
 #>num = 10000 ##int(input('Enter the number of random samples: '))
-vrange = 10.**(np.linspace(MW.v_min,MW.v_max,num=100000.))
 
 print 'mass: ', DM.mass
 print 'v_min: ', MW.v_min
 print 'v_max: ', MW.v_max
 print 'temp: ', DM.temp.cgs
 print 'vrange: ', vrange[0], 'to', vrange[-1]
+plt.show()
 
 #>theoretical = dmvdf.pdf(DM.mass, vrange, DM.temp)
 #>print 'theoretical max: ', max(theoretical.to(units.s/units.km))
